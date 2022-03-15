68c17143df70a75146522a5a41111df49be19678
JinLongNiu <niujinlong@matrixtime.com>
2022-03-15 20:57:49 +0800

    update

---
+++ src/njl_study/可复用&组合/路由/Home.vue
@@ -0,0 +1,28 @@
+<template>
+
+</template>
+
+<script>
+  export default {
+    computed: {
+      username() {
+        // 我们很快就会看到 `params` 是什么
+        return this.$route.params.username
+      },
+    },
+    methods: {
+      goToDashboard() {
+        if (isAuthenticated) {
+          this.$router.push('/dashboard')
+        }
+        else {
+          this.$router.push('/login')
+        }
+      },
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
